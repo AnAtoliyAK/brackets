8c82b528808f5759b90c8d29858719d328877cdf
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-03-13 21:32:41 +0200

    feat: check  func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   const items = bracketsConfig.map((arr)  => arr.join(''));
+   let length1 = 0;
+    
+  
+   do {
+         length1 = str.length;
+         items.forEach((x )=> str = str.replace(x,''));
+
+     } 
+     while (length1 != str.length)
+     return !str.length;
 }
